c4e4c38c7e12b06ddeb1eead406be98af16f75f3
EMN-CSharp <ezequielmenent@hotmail.com>
2022-04-19 22:56:33 -0300

    Add drive events

--- EMNSystemInfo.HardwareAPI/PhysicalStorage/StorageDrives.cs
+++ EMNSystemInfo.HardwareAPI/PhysicalStorage/StorageDrives.cs
@@ -7,30 +7,48 @@
 
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Management;
 
 namespace EMNSystemInfo.HardwareAPI.PhysicalStorage
 {
     /// <summary>
+    /// Main event handler for drive events (insert, remove)
+    /// </summary>
+    /// <param name="drive">The drive that's been inserted or removed</param>
+    public delegate void DriveEventHandler(Drive drive);
+
+    /// <summary>
     /// Class that represents a list of physical drives connected to the system. Administrator privileges are required.
     /// </summary>
     public static class StorageDrives
     {
+        private static ManagementEventWatcher _drvInsertedEvent;
+        private static ManagementEventWatcher _drvRemovedEvent;
+        private static List<Drive> _drives = new();
+
         /// <summary>
         /// Drives list
         /// </summary>
-        public static Drive[] List { get; private set; } = Array.Empty<Drive>();
+        public static Drive[] List { get; private set; } = _drives.ToArray();
 
         /// <summary>
         /// Loads all the connected drives into the <see cref="List"/> property.
         /// </summary>
         public static void LoadDrives()
         {
+            _drvInsertedEvent = new ManagementEventWatcher("SELECT * FROM __InstanceCreationEvent WITHIN 1 WHERE TargetInstance ISA 'Win32_DiskDrive'");
+            _drvInsertedEvent.EventArrived += _drvInsertedEvent_EventArrived;
+            _drvInsertedEvent.Start();
+
+            _drvRemovedEvent = new ManagementEventWatcher("SELECT * FROM __InstanceDeletionEvent WITHIN 1 WHERE TargetInstance ISA 'Win32_DiskDrive'");
+            _drvRemovedEvent.EventArrived += _drvRemovedEvent_EventArrived;
+            _drvRemovedEvent.Start();
+
             //https://docs.microsoft.com/en-us/windows/win32/cimwin32prov/win32-diskdrive
             using var diskDriveSearcher = new ManagementObjectSearcher("SELECT * FROM Win32_DiskDrive") { Options = { Timeout = TimeSpan.FromSeconds(10) } };
             using ManagementObjectCollection diskDrives = diskDriveSearcher.Get();
 
-            List<Drive> drives = new();
             foreach (ManagementBaseObject diskDrive in diskDrives)
             {
                 // Use WMIStorageInfo when Win32 info can't be accessed because of privileges
@@ -41,24 +59,74 @@ namespace EMNSystemInfo.HardwareAPI.PhysicalStorage
                     var instance = Drive.CreateInstance(wmiInfo);
                     if (instance != null)
                     {
-                        drives.Add(instance);
+                        _drives.Add(instance);
                     }
                 }
             }
-
-            List = drives.ToArray();
         }
 
         /// <summary>
-        /// Frees the resources used by <see cref="Drive"/> instances.
+        /// Frees the resources used by <see cref="Drive"/> instances and closes the listeners for drive events.
         /// </summary>
         public static void DisposeDrives()
         {
+            _drvInsertedEvent?.Stop();
+            if (_drvInsertedEvent != null)
+                _drvInsertedEvent.EventArrived -= _drvInsertedEvent_EventArrived;
+            _drvInsertedEvent?.Dispose();
+
+            _drvRemovedEvent?.Stop();
+            if (_drvRemovedEvent != null)
+                _drvRemovedEvent.EventArrived -= _drvRemovedEvent_EventArrived;
+            _drvRemovedEvent?.Dispose();
+
             foreach (Drive storage in List)
                 storage.Close();
 
-            List = Array.Empty<Drive>();
+            _drives.Clear();
         }
+
+        private static void _drvInsertedEvent_EventArrived(object sender, EventArrivedEventArgs e)
+        {
+            ManagementBaseObject driveObject = (ManagementBaseObject)e.NewEvent["TargetInstance"];
+            WMIStorageInfo wmiInfo = new(driveObject);
+
+            if (wmiInfo.DeviceId != null)
+            {
+                Drive newDrive = Drive.CreateInstance(wmiInfo);
+                if (newDrive != null)
+                {
+                    _drives.Add(newDrive);
+                    DriveInserted?.Invoke(newDrive);
+                }
+            }
+        }
+
+        private static void _drvRemovedEvent_EventArrived(object sender, EventArrivedEventArgs e)
+        {
+            ManagementBaseObject driveObject = (ManagementBaseObject)e.NewEvent["TargetInstance"];
+            WMIStorageInfo wmiInfo = new(driveObject);
+
+            Drive removedDrive = (from d in _drives
+                                  where d.Index == wmiInfo.Index
+                                  select d).FirstOrDefault();
+
+            if (removedDrive != default)
+            {
+                _drives.Remove(removedDrive);
+                DriveRemoved?.Invoke(removedDrive);
+            }
+        }
+
+        /// <summary>
+        /// Drive inserted event. This event is <strong>asynchronous</strong>.
+        /// </summary>
+        public static event DriveEventHandler DriveInserted;
+
+        /// <summary>
+        /// Drive removed event. This event is <strong>asynchronous</strong>.
+        /// </summary>
+        public static event DriveEventHandler DriveRemoved;
 
         internal class WMIStorageInfo : StorageInfo
         {
