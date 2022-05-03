6e61fc8bd6fa582d6d9bcef005927cf08466fe19
EMN-CSharp <ezequielmenent@hotmail.com>
2022-05-03 15:45:37 -0300

    Make version info publicly available through the VersionInfo static class

---
+++ EMNSystemInfo.HardwareAPI/VersionInfo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace EMNSystemInfo.HardwareAPI
+{
+    /// <summary>
+    /// Class that represents the library version info
+    /// </summary>
+    public static class VersionInfo
+    {
+        /// <summary>
+        /// Gets a string literal representing the library version
+        /// </summary>
+        public const string Version = "0.2.4.1";
+
+        /// <summary>
+        /// Gets a <see cref="System.Version"/> instance based on <see cref="Version"/> string literal.
+        /// </summary>
+        public static Version GetVersion() => new(Version);
+
+        /// <summary>
+        /// Gets the library build date
+        /// </summary>
+        public static DateTime BuildDate => new(2022, 5, 3);
+    }
+}
