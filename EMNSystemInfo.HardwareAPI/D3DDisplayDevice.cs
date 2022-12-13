dbb0a69ee660416f54825f00fa838fbeb2b4f6b1
EMN-CSharp <ezequielmenent@hotmail.com>
2022-12-13 00:27:17 -0300

    Update copyright notices

--- EMNSystemInfo.HardwareAPI/D3DDisplayDevice.cs
+++ EMNSystemInfo.HardwareAPI/D3DDisplayDevice.cs
@@ -1,7 +1,6 @@
 ﻿// This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0.
 // If a copy of the MPL was not distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
-// Copyright (C) EMN-CSharp and Contributors.
-// Partial Copyright (C) LibreHardwareMonitor and Contributors.
+// Copyright (C) LibreHardwareMonitor and Contributors.
 // All Rights Reserved.
 // Ported from: https://github.com/processhacker/processhacker/blob/master/plugins/ExtendedTools/gpumon.c
 
